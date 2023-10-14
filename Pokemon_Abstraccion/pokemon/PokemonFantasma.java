faad241ec2a53508a5417df6ceda318e4cd7f8a0
LizandroAdolfoFranciscoCaamal <145051616+LizandroAdolfoFranciscoCaamal@users.noreply.github.com>
2023-10-13 18:33:00 -0600

    PokemonFantasma.java

--- Pokemon_Abstraccion/pokemon/PokemonFantasma.java
+++ Pokemon_Abstraccion/pokemon/PokemonFantasma.java
@@ -3,9 +3,27 @@ package pokemon;
 public class PokemonFantasma extends Pokemon {
     public PokemonFantasma(String nombre, int nivel) {
     super(nombre, "Pokemon fantasma", nivel);
-}
-  @Override 
-  public double obtenerEfectividad (Pokemon pokemon) { 
-      return 1.0; 
+
+ListaMovimientos listaMovimientos = new ListaMovimientos();
+
+setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("Puño sombra"));
+setMovimientos(1, listaMovimientos.buscarMovimientoPorNombre("Rayo confuso"));
+setMovimientos(2, listaMovimientos.buscarMovimientoPorNombre("Meteoros"));
+setMovimientos(3, listaMovimientos.buscarMovimientoPorNombre("Garra brutal"));
+    }
+
+    @Override 
+    public double obtenerEfectividad (Pokemon pokemon) { 
+      double efectividad = 1.0;
+        
+      if(pokemon.getTipo()== Tipo.PSIQUICO) efectividad = 2.0;
+      if(pokemon.getTipo()== Tipo.FANTASMA) efectividad = 2.0;
+      if(pokemon.getTipo()== Tipo.BICHO) efectividad = 1.0;
+      if(pokemon.getTipo()== Tipo.SINIESTRO) efectividad = 0.5;
+      if(pokemon.getTipo()== Tipo.ACERO) efectividad = 0.5;
+      if(pokemon.getTipo()== Tipo.LUCHA) efectividad = 0.0;
+      if(pokemon.getTipo()== Tipo.NORMAL) efectividad = 0.0;
+  
+      return efectividad;
   }
 }
