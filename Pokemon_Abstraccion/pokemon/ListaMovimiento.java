222de55320b24b35ef0f40eff6934d7bc790963c
LizandroAdolfoFranciscoCaamal <145051616+LizandroAdolfoFranciscoCaamal@users.noreply.github.com>
2023-10-13 18:35:40 -0600

    ListaMovimiento.java

--- Pokemon_Abstraccion/pokemon/ListaMovimiento.java
+++ Pokemon_Abstraccion/pokemon/ListaMovimiento.java
@@ -1,3 +1,5 @@
+package pokemon;
+
 import java.util.ArrayList;
 
 public class ListaMovimientos {
@@ -10,12 +12,13 @@ public class ListaMovimientos {
     movimientos.add(new Movimiento ("Bola voltio", 60, Tipo.ELECTRICO, 10));
 
     // Movimientos de tipo Fantasma
-    movimientos.add(new Movimiento ("Puño sombra", 60, Tipo.FANTASMA 18));
+    movimientos.add(new Movimiento ("Puño sombra", 60, Tipo.FANTASMA, 18));
     movimientos.add(new Movimiento ("Rayo confuso", 100, Tipo.FANTASMA, 5)); 
 
     // Movimientos de tipo normal
-    Movimientos.add(new Movimiento ("Meteoros", 68, Tipo. NORMAL, 20));
-
+    movimientos.add(new Movimiento ("Meteoros", 68, Tipo. NORMAL, 20));
+    movimientos.add(new Movimiento ("Garra brutal", 75, Tipo.NORMAL, 10));
+    }
     public Movimiento buscarMovimientoPorNombre(String nombre) {
         for (Movimiento movimiento: movimientos) { 
             if (movimiento.getNombre().equals(nombre)) {
@@ -23,5 +26,6 @@ public class ListaMovimientos {
             }
         }
         return null;
+        
     }
 }
