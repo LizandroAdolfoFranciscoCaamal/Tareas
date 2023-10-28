8740272c28c90662ff00b2de9c4ee01bc09bc55f
LizandroAdolfoFranciscoCaamal <145051616+LizandroAdolfoFranciscoCaamal@users.noreply.github.com>
2023-10-27 22:38:22 -0600

    ListaMovimiento.java

--- Pokemon_Polimorfismo/Pokemon/ListaMovimiento.java
+++ Pokemon_Polimorfismo/Pokemon/ListaMovimiento.java
@@ -8,16 +8,16 @@ public class ListaMovimientos {
     public ListaMovimientos() {
 
         // Movimientos de tipo electrico
-        movimientos.add(new Movimiento("Puño Trueno", 75, Tipo.ELECTRICO, 38));
-        movimientos.add(new Movimiento("Bola voltio", 60, Tipo.ELECTRICO, 10));
+        movimientos.add(new Movimiento("Puño Trueno", 75, Tipo.ELECTRICO, 38, TipoMovimiento.FISICO));
+        movimientos.add(new Movimiento("Bola voltio", 60, Tipo.ELECTRICO, 10, TipoMovimiento.ESPECIAL));
 
         // Movimientos de tipo Fantasma
-        movimientos.add(new Movimiento("Puño sombra", 60, Tipo.FANTASMA, 18));
-        movimientos.add(new Movimiento("Rayo confuso", 100, Tipo.FANTASMA, 5));
+        movimientos.add(new Movimiento("Puño sombra", 60, Tipo.FANTASMA, 18, TipoMovimiento.FISICO));
+        movimientos.add(new Movimiento("Rayo confuso", 100, Tipo.FANTASMA, 5,TipoMovimiento.ESTADO));
 
         // Movimientos de tipo normal
-        movimientos.add(new Movimiento("Meteoros", 68, Tipo.NORMAL, 20));
-        movimientos.add(new Movimiento("Garra brutal", 75, Tipo.NORMAL, 10));
+        movimientos.add(new Movimiento("Meteoros", 68, Tipo.NORMAL, 20, TipoMovimiento.ESPECIAL));
+        movimientos.add(new Movimiento("Garra brutal", 75, Tipo.NORMAL, 10, TipoMovimiento.FISICO));
     }
 
     public Movimiento buscarMovimientoPorNombre(String nombre) {
