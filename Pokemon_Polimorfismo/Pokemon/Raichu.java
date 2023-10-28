81c6d6add3b3ae1c37f05b4d759c58010a599c47
LizandroAdolfoFranciscoCaamal <145051616+LizandroAdolfoFranciscoCaamal@users.noreply.github.com>
2023-10-27 18:39:39 -0600

    Raichu.java

---
+++ Pokemon_Polimorfismo/Pokemon/Raichu.java
@@ -0,0 +1,34 @@
+package pokemon;
+
+public class Raichu extends Pokemon {
+    public Raichu(String nombre, int nivel) {
+        super(nombre, Tipo.Electrico, nivel);
+
+        ListaMovimientos listaMovimientos = new ListaMovimientos();
+
+        setMovimientos(0, listaMovimientos.buscarMovimientoPorNombre("Puño Trueno"));
+        setMovimientos(1, listaMovimientos.buscarMovimientoPorNombre("Bola voltio "));
+        setMovimientos(2, listaMovimientos.buscarMovimientoPorNombre("Meteoros"));
+        setMovimientos(3, listaMovimientos.buscarMovimientoPorNombre("Garra brutal"));
+    }
+
+    @Override
+    public double obtenerEfectividad(Pokemon pokemon) {
+        double efectividad = 1.0;
+
+        if (pokemon.getTipo() == Tipo.AGUA)
+            efectividad = 2.0;
+        if (pokemon.getTipo() == Tipo.VOLADOR)
+            efectividad = 2.0;
+        if (pokemon.getTipo() == Tipo.ACERO)
+            efectividad = 1.0;
+        if (pokemon.getTipo() == Tipo.ELECTRICO)
+            efectividad = 0.5;
+        if (pokemon.getTipo() == Tipo.PLANTA)
+            efectividad = 0.5;
+        if (pokemon.getTipo() == Tipo.TIERRA)
+            efectividad = 0.0;
+
+        return efectividad;
+    }
+}
